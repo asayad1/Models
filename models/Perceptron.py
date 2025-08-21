da762f59159919e21ab6410bf027b7891487cd86
Ahmad Sayad <ahmadksayad@gmail.com>
2025-08-21 00:24:22 -0400

    Perceptron with epochs

--- models/Perceptron.py
+++ models/Perceptron.py
@@ -13,26 +13,26 @@ class Perceptron:
     def activation(self, activity: float):
         return 1 if activity >= 0 else 0 
 
-    def fit(self, X: pd.DataFrame, y: pd.Series, learning_rate: float):
+    def fit(self, *, X: pd.DataFrame, y: pd.Series, learning_rate: float, epochs: int):
         # Initialize the weights and bias to 0 
         self.weights = [0] * X.shape[1]
         self.bias = 0
 
-        # For each training example:
-        # 1) Compute the output value: w1 x1 + w2 x2 + .. + wn xn + b
-        # 2) Update the weights and bias unit
-
-        for i in range(X.shape[0]):
-            activity = 0
-            for x in range(X.shape[1]):
-                activity += self.weights[x] * X.iloc[:, x][i]
-            activity += self.bias
-            
-            y_prediction = self.activation(activity)
-            
-            for x in range(X.shape[1]):
-                self.weights[x] += learning_rate * (y[i] - y_prediction) * X.iloc[:, x][i]
-            self.bias += learning_rate * (y[i] - y_prediction)
+        for _ in range(epochs):
+            # For each training example:
+            # 1) Compute the output value: w1 x1 + w2 x2 + .. + wn xn + b
+            # 2) Update the weights and bias unit
+            for i in range(X.shape[0]):
+                activity = 0
+                for x in range(X.shape[1]):
+                    activity += self.weights[x] * X.iloc[:, x][i]
+                activity += self.bias
+                
+                y_prediction = self.activation(activity)
+                
+                for x in range(X.shape[1]):
+                    self.weights[x] += learning_rate * (y[i] - y_prediction) * X.iloc[:, x][i]
+                self.bias += learning_rate * (y[i] - y_prediction)
 
 
     def predict(self, X: pd.DataFrame):
@@ -50,14 +50,11 @@ class Perceptron:
 if __name__ == '__main__':
     dataset_url = 'https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/iris.csv'
     dataset = pd.read_csv(dataset_url)
-
-    for i in dataset:
-        print(i)
     
     X = dataset.drop('species', axis=1)
     y = (dataset['species'] == 'setosa').astype(int)
 
     p = Perceptron()
-    p.fit(X, y, 1)
+    p.fit(X=X, y=y, learning_rate=1, epochs=500)
 
     print(p.weights, p.bias)
