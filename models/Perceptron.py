85005f766f12b0bd100936b7584d319342842bac
Ahmad Sayad <ahmadksayad@gmail.com>
2025-08-21 14:40:24 -0400

    Perceptron early convergence check

--- models/Perceptron.py
+++ models/Perceptron.py
@@ -9,23 +9,32 @@ from typing import List
 class Perceptron:
     weights: pd.Series = None
     bias: float = None 
+    errors: List[int] = None
 
     def activation(self, activity: float):
         return 1 if activity >= 0 else 0 
 
     def fit(self, *, X: pd.DataFrame, y: pd.Series, learning_rate: float, epochs: int):
         # Initialize the weights and bias to 0 
-        self.weights = pd.Series(0, index=X.columns)
+        self.weights = pd.Series(0.0, index=X.columns)
         self.bias = 0
-
+        
         for _ in range(epochs):
             # For each training example:
             # 1) Compute the output value: w1 x1 + w2 x2 + .. + wn xn + b
             # 2) Update the weights and bias unit
+            self.errors = []
+
             for i in range(X.shape[0]):
                 y_prediction = self.predict(X.iloc[i])
-                self.weights += learning_rate * (y[i] - y_prediction) * X.iloc[i]
-                self.bias += learning_rate * (y[i] - y_prediction)
+                error = y[i] - y_prediction
+                self.weights += learning_rate * error * X.iloc[i]
+                self.bias += learning_rate * error
+                self.errors.append(abs(error))
+            
+            # See if we have converged early
+            if not (1 in self.errors): break 
+
 
     def predict(self, X: pd.Series) -> float:
         activity = X.dot(self.weights) + self.bias
@@ -41,6 +50,6 @@ if __name__ == '__main__':
     y = (dataset['species'] == 'setosa').astype(int)
 
     p = Perceptron()
-    p.fit(X=X, y=y, learning_rate=1, epochs=100)
+    p.fit(X=X, y=y, learning_rate=1, epochs=500)
     
     print(p.weights, p.bias)
