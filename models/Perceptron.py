f9d5ea242a1f3d3f0c18b05dc80a2720977e2cbc
Ahmad Sayad <ahmadksayad@gmail.com>
2025-08-21 00:20:59 -0400

    Single pass perceptron

--- models/Perceptron.py
+++ models/Perceptron.py
@@ -8,22 +8,43 @@ from typing import List
 
 class Perceptron:
     weights: List[float] = None
+    bias: float = None 
 
-    def __init__(self):
-        pass
+    def activation(self, activity: float):
+        return 1 if activity >= 0 else 0 
 
-
-    def fit(self, X: pd.DataFrame, y: pd.Series):
+    def fit(self, X: pd.DataFrame, y: pd.Series, learning_rate: float):
         # Initialize the weights and bias to 0 
-    
+        self.weights = [0] * X.shape[1]
+        self.bias = 0
+
         # For each training example:
         # 1) Compute the output value: w1 x1 + w2 x2 + .. + wn xn + b
         # 2) Update the weights and bias unit
 
-        pass
+        for i in range(X.shape[0]):
+            activity = 0
+            for x in range(X.shape[1]):
+                activity += self.weights[x] * X.iloc[:, x][i]
+            activity += self.bias
+            
+            y_prediction = self.activation(activity)
+            
+            for x in range(X.shape[1]):
+                self.weights[x] += learning_rate * (y[i] - y_prediction) * X.iloc[:, x][i]
+            self.bias += learning_rate * (y[i] - y_prediction)
 
-    def predict(self):
-        pass
+
+    def predict(self, X: pd.DataFrame):
+        activity = 0
+        for x in range(X.shape[1]):
+            activity += self.weights[x] * X.iloc[:, x][0]
+        activity += self.bias
+
+    
+        y_prediction = self.activation(activity)
+        return y_prediction
+            
 
 
 if __name__ == '__main__':
@@ -32,4 +53,11 @@ if __name__ == '__main__':
 
     for i in dataset:
         print(i)
-    print(dataset.head())
+    
+    X = dataset.drop('species', axis=1)
+    y = (dataset['species'] == 'setosa').astype(int)
+
+    p = Perceptron()
+    p.fit(X, y, 1)
+
+    print(p.weights, p.bias)
